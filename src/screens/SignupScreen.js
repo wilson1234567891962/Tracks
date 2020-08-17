eac765a095fd4f86f6525ead6bf85de819e65bc7
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-17 17:59:38 -0500

    Create component NAV LINK

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import { View, StyleSheet } from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext);
@@ -16,13 +15,10 @@ const SignupScreen = ({ navigation }) => {
         submitButtonText="Sign Up"
         onSubmit={signup}
       />
-      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-        <Spacer>
-          <Text style={styles.link}>
-            Already have an account? Sign in instead
-          </Text>
-        </Spacer>
-      </TouchableOpacity>
+      <NavLink
+        routeName="Signin"
+        text="Already have an account? Sign in instead!"
+      />
     </View>
   );
 };
@@ -37,10 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    marginBottom: 150,
-  },
-  link: {
-    color: 'blue',
+    marginBottom: 250,
   },
 });
 
