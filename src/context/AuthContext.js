698885f879f998e11341bb61fe8c59528e91dc97
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-17 17:20:37 -0500

    Create the method AsyncStorage to save the token

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,28 +1,32 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
+import { AsyncStorage } from "react-native";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.paylodad };
-      break;
+    case "signup":
+      return { errorMessage: "", token: action.paylodad };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post("/signup", { email, password });
-      console.log(response.data);
-    } catch (err) {
-      dispatch({
-        type: "add_error",
-        paylodad: "Something went wrin with sign up",
-      });
-    }
-  };
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signup", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({
+      type: "signup",
+      paylodad: response.data.token,
+    });
+  } catch (err) {
+    dispatch({
+      type: "add_error",
+      paylodad: "Something went wrin with sign up",
+    });
+  }
 };
 
 const signin = (dispatch) => {
@@ -42,5 +46,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false, errorMessage: "" }
+  { token: null, errorMessage: "" }
 );
