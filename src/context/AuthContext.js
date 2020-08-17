0e2a53a581fa88fc8e4587f072f181f1130da72f
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-17 16:24:04 -0500

    Create the methods to signin signup and logout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,6 +7,27 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // Make api request to sign up with that email and password
+    // if we sign up, modify our state, and say that we are authenticated
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // try to signin
+    // hanlde success by updating state
+    // handle failure by showing error message
+  };
+};
+
+const signout = (dispatch) => {
+  return ({ email, password }) => {
+    // signout
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
   {},
