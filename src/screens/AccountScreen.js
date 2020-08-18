5ad5b33ba6671b8d04982bfbc193ade06c881ce2
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-18 10:42:58 -0500

    Change the AccountScreen component and logout method

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
-const AccountScren = () => {
-    return <Text>Account Screen</Text>
+const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
 
-export default AccountScren;
+export default AccountScreen;
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    marginBottom: 250,
+    marginBottom: 150,
   },
 });
 
